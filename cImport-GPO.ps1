ef655dac81b94a3bbfe7e59a75e7439646804c92
Alex <ahivent@gmail.com>
2017-12-22 22:20:50 -0600

    Add files via upload
    Importing GPO's from a different forest

---
+++ cImport-GPO.ps1
@@ -0,0 +1,20 @@
+﻿#$GPOs = Get-GPO -all
+$GPOGUIDs = Get-ChildItem -Path C:\Users\admin01\Downloads\GPOs\GPOs
+$arrayGPOExclusion = ("Default Domain Controllers Policy" , "Default Domain Policy")
+ForEach ($GPOGUID in $GPOGUIDs){
+    [xml]$XmlDocument=Get-Content -Path  C:\Users\admin01\Downloads\GPOs\GPOs\$GPOGUID\gpreport.xml
+    if (($XmlDocument.gpo.Name -ne "Default Domain Controllers Policy") -and ($XmlDocument.gpo.Name -ne "Default Domain Policy")){
+        $XmlDocument.gpo.Name
+        #Import-GPO -BackupGpoName $GPOGUID.Name -TargetGUID $GPOGUID.Name -CreateIfNeeded -Path C:\Users\admin01\Downloads\GPOs\GPOs -WhatIf
+        Import-GPO -BackupId $GPOGUID.Name -TargetName $XmlDocument.gpo.Name -CreateIfNeeded -Path C:\Users\admin01\Downloads\GPOs\GPOs
+        }
+    elseif ($XmlDocument.gpo.Name -in $arrayGPOExclusion){ # careful with arrays and conditional logic; i.e. -in vs eq,contains
+        "---Skipping: " + $XmlDocument.gpo.Name
+        }
+    else{
+        Write-Host "Exception has occured"
+        }
+}
+# Report the new GPO List
+Write-Host `n`n"__________________"`n"New list of GPOs:"`n"__________________"
+Get-GPO -all | Select-Object -Property DisplayName | Format-Table
