7af21212287dd3085abbbc8ae7dfcdf16c468aaa
Alex <ahivent@gmail.com>
2017-12-22 22:41:20 -0600

    Update cImport-GPO.ps1
    Both conditional logic statements now evaluate the arrayGPOExclusion

--- cImport-GPO.ps1
+++ cImport-GPO.ps1
@@ -4,7 +4,7 @@ $GPOGUIDs = Get-ChildItem -Path C:\Users\admin01\Downloads\GPOs\GPOs
 $arrayGPOExclusion = ("Default Domain Controllers Policy" , "Default Domain Policy")
 ForEach ($GPOGUID in $GPOGUIDs){
     [xml]$XmlDocument=Get-Content -Path  C:\Users\admin01\Downloads\GPOs\GPOs\$GPOGUID\gpreport.xml
-    if (($XmlDocument.gpo.Name -ne "Default Domain Controllers Policy") -and ($XmlDocument.gpo.Name -ne "Default Domain Policy")){
+    if ($XmlDocument.gpo.Name -notin $arrayGPOExclusion){
         $XmlDocument.gpo.Name
         #Import-GPO -BackupGpoName $GPOGUID.Name -TargetGUID $GPOGUID.Name -CreateIfNeeded -Path C:\Users\admin01\Downloads\GPOs\GPOs -WhatIf
         Import-GPO -BackupId $GPOGUID.Name -TargetName $XmlDocument.gpo.Name -CreateIfNeeded -Path C:\Users\admin01\Downloads\GPOs\GPOs
